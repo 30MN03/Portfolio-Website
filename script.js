7a3dfbf2df054319ea1e8f80e79e5bc1c931e97b
Mishal Nanalal <mishal@holibob.tech>
2024-03-25 23:15:23 +0000

    Further refining to Layout

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('.header');
-    const sidebar = document.querySelector('.sidebar');
-    if (window.scrollY > header.offsetHeight) {
-        header.classList.add('scrolled');
-        sidebar.style.left = "0"; // Show sidebar
-    } else {
-        header.classList.remove('scrolled');
-        sidebar.style.left = "-250px"; // Hide sidebar
-    }
-});
+// window.addEventListener('scroll', function() {
+//     const header = document.querySelector('.header');
+//     const sidebar = document.querySelector('.sidebar');
+//     if (window.scrollY > header.offsetHeight) {
+//         header.classList.add('scrolled');
+//         sidebar.style.left = "0"; // Show sidebar
+//     } else {
+//         header.classList.remove('scrolled');
+//         sidebar.style.left = "-250px"; // Hide sidebar
+//     }
+// });
 
 document.addEventListener("DOMContentLoaded", function() {
     const sidebar = document.querySelector('.sidebar');
@@ -29,27 +29,4 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Update padding when sidebar visibility changes
     document.addEventListener('scroll', adjustPadding);
-});
-
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
-
-    // Get form values
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var message = document.getElementById('message').value;
-
-    // Here you can implement your logic for form submission (e.g., sending data to a server)
-
-    // For demonstration purposes, let's just log the values to the console
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console.log('Message:', message);
-
-    // Clear form fields
-    document.getElementById('name').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('message').value = '';
-
-    alert('Message sent successfully!');
 });
